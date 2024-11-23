513c24ce5e4da65c4c92ef21f859e139fb3ef71f
Roman Zimmermann <roman@more-onion.com>
2024-11-23 17:16:56 +0100

    More events, with defaults and better dates

--- src/ftw_hub/__init__.py
+++ src/ftw_hub/__init__.py
@@ -34,12 +34,12 @@ def cli(ctx, data_dir: pathlib.Path):
                 new_data = yaml.safe_load(f)
                 data = utils.merge_dicts(data, new_data)
     ctx.obj["data"] = data
-    ctx.obj["events"] = list(
-        sorted(
-            (format_event(e, data.get("series", {})) for e in data.get("events", [])),
-            key=lambda e: e["sort_date"],
-        )
-    )
+    events = [format_event(e, data.get("series", {})) for e in data.get("events", [])]
+    ws_data = {"workshop": True, "social": False, "sub_event": True}
+    ws = [
+        utils.merge_dicts(e, e["workshop_event"], ws_data) for e in events if "workshop_event" in e
+    ]
+    ctx.obj["events"] = list(sorted(events + ws, key=lambda e: e["sort_date"]))
 
 
 link_data = collections.defaultdict(lambda: {"symbol": "link", "alt": "Link-Symbol"})
@@ -73,9 +73,17 @@ MONTHS_DE = [
     "Dezember",
 ]
 
+EVENT_DEFAULTS = {
+    "description": "",
+    "links": {},
+    "workshop": False,
+    "social": False,
+}
+
 
 def format_event(event: dict, series: dict):
     """Extend the event data structure a bit."""
+    event = utils.merge_dicts(EVENT_DEFAULTS, event)
     if (series_key := event.get("series")) and (event_series := series.get(series_key)):
         event = utils.merge_dicts(event_series.get("defaults", {}), event)
     event["links"] = [{"href": l, **link_data[t]} for t, l in event["links"].items() if l]
@@ -94,16 +102,19 @@ def format_date_range(
     if isinstance(start, datetime.datetime):
         start_time = start.strftime("%H:%M")
         if end:
-            result += f" {start_time}{range_word}{end.strftime('%H:%M')},"
+            result += f", {start_time}{range_word}{end.strftime('%H:%M')},"
         else:
             result += f" ab {start_time},"
     else:
         if end and (end > start):
-            result += f"{range_word}{end.strftime('%d.%m')}"
+            result += f"{range_word}{end.strftime('%d.%m.')},"
+        else:
+            result += ","
     return result
 
 
 def sort_date(d: datetime.date | datetime.datetime) -> datetime.datetime:
+    """Convert a date to a datetime object."""
     if isinstance(d, datetime.datetime):
         return d
     return datetime.datetime.combine(d, datetime.datetime.min.time())
@@ -130,11 +141,15 @@ def monatsuebersicht_html(ctx, month):
     for event in ctx.obj["events"]:
         if event["sort_date"] < date_from:
             continue
-        key = "preview" if event["sort_date"] >= date_to else "events"
+        if event["sort_date"] >= date_to:
+            if not event.get("sub_event", False):
+                key = "preview"
+            else:
+                continue
+        else:
+            is_workshop = event.get("workshop", False) and not event.get("social", True)
+            key = "workshops" if is_workshop else "events"
         tpl_data[key].append(event)
-    for event in tpl_data["events"]:
-        if ws := event.get("workshop_event", None):
-            tpl_data["workshops"].append(utils.merge_dicts(event, ws))
 
     template = ctx.obj["jinja_env"].get_template("monatsuebersicht.html")
     click.echo(template.render(tpl_data), nl=False)
@@ -154,6 +169,8 @@ def folktanz_at(ctx):
     }
     for event in ctx.obj["events"]:
         if sort_date(event["sort_date"]) < date_from:
+            continue
+        if event.get("sub_event", False):
             continue
         format_date_range(event["start"], event.get("end"))
         month = event["start"].strftime("%y-%m")
